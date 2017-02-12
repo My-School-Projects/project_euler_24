34720214fb8f34e278d5d806a4cbe64985866eaf
Michael Dorst <code@mikefdorst.com>
2017-02-12 04:53:59 -0800

    Add lots of documenting comments

--- src/main.rs
+++ src/main.rs
@@ -39,8 +39,29 @@ fn permutation(p: u64, s: &Vec<u64>) -> Vec<u64>
 }
 
 ///
-/// Given a sequence S, this function finds the first element
+/// Given a sequence S, this function finds the first element e[0]
 /// of the sequence P(S), which is the pth lexicographic permutation of S.
+///
+/// e[0] is given by  p mod n! / (n - 1)!, where p is the permutation,
+/// and n is the size of the sequence.
+///
+/// This works because the permutations are listed in lexical order,
+/// and so is the sequence S. As p increases from 0 to n, the first element e[0]
+/// starts at S[0] (the first element of S), and repeats (n-1)! times before
+/// becoming S[1], and repeating (n-1)! again, and so on. In this way, we can
+/// identify groupings where e[0] is the same value for certain ranges of p.
+/// Because there are (n-1)! permutations in each group, for any valid p,
+/// e[0] can be found by taking  p / (n - 1)!.
+///
+/// Unfortunately there's a slight problem. Because this function will be called on
+/// ever decreasing sizes of n, p will eventually be too large to be considered
+/// a permutation of S. To fix this, we need to make the "permutation space" wrap around.
+/// In other words, if there are only 24 possible permutations of S, and p = 28, we need
+/// p to refer to the 4th permutation of S. To accomplish that we must take p mod 24, or n!.
+/// So our actual formula for the first element of S, e[0] is  p mod n! / (n-1)!.
+///
+/// Note: we actually use p-1, not p. This is just because I defined the permutations to
+/// start at 0, not 1, so I have to adjust it to make the value come out as expected.
 ///
 fn first_element_of_permutation(p: u64, s: &Vec<u64>) -> u64
 {
@@ -61,13 +82,24 @@ fn factorial(n: u64) -> u64
 }
 
 ///
-/// RemoveFirst is provided simply as a succinct way
+/// RemoveFirst is provided as a succinct way
 /// to remove the first occurrence of an element from a vec.
+///
+/// If you're not familiar with Rust syntax, basically this is just
+/// an extension to Vec.
+/// First you declare a trait, which can be applied to any class
+/// if you're willing to take the time to write the class-specific implementation.
 ///
 trait RemoveFirst<E> {
     fn remove_first(&mut self, e: E);
 }
 
+///
+/// This is the class specific implementation for the trait RemoveFirst.
+/// It only applies to Vec's which hold types that implement the PartialEq trait,
+/// and the Copy trait. This is because the elements need to be comparable, and
+/// they need to be copyable in order to use the iter().position() method.
+///
 impl <E: PartialEq<E> + Copy> RemoveFirst<E> for Vec<E>
 {
     fn remove_first(&mut self, e: E)
